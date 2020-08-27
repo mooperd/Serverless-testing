4adf1d2d11f938995b7aa5cfa0111ad38afd2961
Andrew Holway <andrew.holway@gmail.com>
2020-08-27 15:19:01 +0200

    trying with s3 bucket

--- handler.py
+++ handler.py
@@ -1,9 +1,12 @@
 import json
 import requests
 import pandas
+import sys
 
 
 def run(event, context):
+    print(os.environ['S3_BUCKET'])
+    print(os.environ['S3_KEY_BASE'])
     url = "https://cache1.phantombooster.com/ukZxdjCQmz0/Vg7zuvUGSEG6yzglJ6jkhA/result.json"
     request = requests.get(url)
     list_of_dicts_data = request.json()
