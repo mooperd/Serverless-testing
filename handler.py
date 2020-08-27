9209d22e074e28101f1fb01fd0049e7ced42eaee
Andrew Holway <andrew.holway@gmail.com>
2020-08-27 16:04:54 +0200

    now with s3

--- handler.py
+++ handler.py
@@ -1,13 +1,19 @@
 import json
 import requests
 import pandas
-import sys
+import arrow
+import base64
+import os
+
+
 
 
 def run(event, context):
     print(os.environ['S3_BUCKET'])
     print(os.environ['S3_KEY_BASE'])
     url = "https://cache1.phantombooster.com/ukZxdjCQmz0/Vg7zuvUGSEG6yzglJ6jkhA/result.json"
-    request = requests.get(url)
-    list_of_dicts_data = request.json()
-    print(len(list_of_dicts_data))
+    df = pandas.read_json(requests.get(url).text)
+    random_string = base64.b64encode(arrow.utcnow().format('SSSSSSSSS').encode('ascii')).decode("utf-8")
+    df.to_csv("s3://{}/{}/{}.csv".format(os.environ['S3_BUCKET'], os.environ['S3_KEY_BASE'], random_string))
+
+
