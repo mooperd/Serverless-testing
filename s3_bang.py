8dcb97e6033aedb688b3ca8bfb8167b46195a6d0
Andrew Holway <andrew.holway@gmail.com>
2020-08-30 17:49:50 +0200

    Update s3_bang.py

--- s3_bang.py
+++ s3_bang.py
@@ -7,6 +7,6 @@ import os
 def s3_bang():
     url = "https://cache1.phantombooster.com/ukZxdjCQmz0/Vg7zuvUGSEG6yzglJ6jkhA/result.json"
     df = pandas.read_json(requests.get(url).text)
-    output_filename = "{}-{}-{}.csv".format(os.environ['PROJECT_NAME'], os.environ['GIT_BRANCH'], os.environ['GIT_SHA'])
+    output_filename = "{}_{}_{}.csv".format(os.environ['PROJECT_NAME'], os.environ['GIT_BRANCH'], os.environ['GIT_SHA'])
     df.to_csv("s3://{}/{}/{}".format(os.environ['S3_BUCKET'], os.environ['S3_KEY_BASE'], output_filename))
 
