d4a61e1744ae32190f61103c8fdd30277257fc37
Andrew Holway <andrew.holway@gmail.com>
2020-08-30 17:41:35 +0200

    better output file naming

--- s3_bang.py
+++ s3_bang.py
@@ -7,7 +7,6 @@ import os
 def s3_bang():
     url = "https://cache1.phantombooster.com/ukZxdjCQmz0/Vg7zuvUGSEG6yzglJ6jkhA/result.json"
     df = pandas.read_json(requests.get(url).text)
-    random_string = base64.b64encode(arrow.utcnow().format('SSSSSSSSS').encode('ascii')).decode("utf-8")
-    output_filename = "{}-{}-{}.csv".format(random_string, os.environ['GIT_BRANCH'], os.environ['GIT_SHA'])
+    output_filename = "{}-{}-{}.csv".format(os.environ['PROJECT_NAME'], os.environ['GIT_BRANCH'], os.environ['GIT_SHA'])
     df.to_csv("s3://{}/{}/{}".format(os.environ['S3_BUCKET'], os.environ['S3_KEY_BASE'], output_filename))
 
