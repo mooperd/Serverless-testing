3190b47ef1215d531a7e4f70574e16f5ff323ec7
Andrew Holway <andrew.holway@gmail.com>
2020-08-30 16:20:33 +0200

    updates

--- s3_bang.py
+++ s3_bang.py
@@ -5,9 +5,10 @@ import base64
 import os
 
 def s3_bang():
+    output-filename = open("lorem.txt", "rt").read() 
     url = "https://cache1.phantombooster.com/ukZxdjCQmz0/Vg7zuvUGSEG6yzglJ6jkhA/result.json"
     df = pandas.read_json(requests.get(url).text)
     random_string = base64.b64encode(arrow.utcnow().format('SSSSSSSSS').encode('ascii')).decode("utf-8")
-    df.to_csv("s3://{}/{}/{}.csv".format(os.environ['S3_BUCKET'], os.environ['S3_KEY_BASE'], random_string))
+    df.to_csv("s3://{}/{}/{}-{}.csv".format(os.environ['S3_BUCKET'], os.environ['S3_KEY_BASE'], random_string, output-filename))
 
         
