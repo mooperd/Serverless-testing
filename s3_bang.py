503a4318b9ced41afd38580714c47685746d2761
Andrew Holway <andrew.holway@gmail.com>
2020-08-30 17:10:57 +0200

    now with branch and sha

--- s3_bang.py
+++ s3_bang.py
@@ -5,10 +5,9 @@ import base64
 import os
 
 def s3_bang():
-    output-filename = open("git-location.txt", "rt").read()
     url = "https://cache1.phantombooster.com/ukZxdjCQmz0/Vg7zuvUGSEG6yzglJ6jkhA/result.json"
     df = pandas.read_json(requests.get(url).text)
     random_string = base64.b64encode(arrow.utcnow().format('SSSSSSSSS').encode('ascii')).decode("utf-8")
-    df.to_csv("s3://{}/{}/{}-{}.csv".format(os.environ['S3_BUCKET'], os.environ['S3_KEY_BASE'], random_string, output-filename))
+    output_filename = "{}-{}-{}.csv".format(random_string, os.environ['GIT_BRANCH'], os.environ['GIT_SHA'])
+    df.to_csv("s3://{}/{}/{}".format(os.environ['S3_BUCKET'], os.environ['S3_KEY_BASE'], output_filename))
 
-        
