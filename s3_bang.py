7594f1a5ec3b62603f2b1b5516418fdddeefde86
Andrew Holway <andrew.holway@gmail.com>
2020-09-02 13:39:19 +0200

    fixed filenames

--- s3_bang.py
+++ s3_bang.py
@@ -7,6 +7,7 @@ import os
 def s3_bang():
     url = "https://cache1.phantombooster.com/ukZxdjCQmz0/Vg7zuvUGSEG6yzglJ6jkhA/result.json"
     df = pandas.read_json(requests.get(url).text)
-    output_filename = "{}_{}_{}.csv".format(os.environ['PROJECT_NAME'], os.environ['GIT_BRANCH'], os.environ['GIT_SHA'])
+    timestamp = arrow.utcnow().timestamp
+    output_filename = "{}_{}_{}_{}.csv".format(os.environ['PROJECT_NAME'], timestamp, os.environ['GIT_BRANCH'], os.environ['GIT_SHA'][0 : 7])
     df.to_csv("s3://{}/{}/{}".format(os.environ['S3_BUCKET'], os.environ['S3_KEY_BASE'], output_filename))
 
